ee15733617854149ad69077b8bf0d6cdcb1ab296
rkali1 <32766998+rkali1@users.noreply.github.com>
2018-04-14 18:19:20 +0100

    Add files via upload
    Update - Code for logout function.

--- Software/welcome.php
+++ Software/welcome.php
@@ -1,44 +1,13 @@
+
+
 <!DOCTYPE html>
-<html lang="en">
+<html>
+<body>
 
-  <head>
+<h1>Successful </h1>
 
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
-    <title>Bradford University - Placement Portal</title>
-
-    <!-- Bootstrap core CSS -->
-    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Custom fonts for this template -->
-    <link href="https://fonts.googleapis.com/css?family=Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Lora:400,400i,700,700i" rel="stylesheet">
-
-    <!-- Custom styles for this template -->
-    <link href="css/business-casual.css" rel="stylesheet">
-
-  </head>
-
-  <body>
-
-    <h1 class="site-heading text-center text-white d-none d-lg-block">
-      <span class="site-heading-upper text-primary mb-3">Bradford University</span>
-      <span class="site-heading-lower">Placement Portal</span>
-    <span class="site-heading-upper text-primary mb-1">Welcome !</span>
-    </h1>
-      
-      <input type="button" class="btn btn-info" value="Enter Student Portal" onclick=" relocate_home()">
-
-<script>
-function relocate_home()
-{
-     location.href = "main.html";
-} 
-</script>
-
-  </body>
-
+</body>
 </html>
+
+
+<div> <a href="logout.php">Logout </a></div>
